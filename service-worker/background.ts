a18c8d7b60ee5a2e72fbd16f6907ecf0b2a4b059
nabedev <26590545+nabedev@users.noreply.github.com>
2022-07-29 20:34:28 +0900

    Don't use popup.html

--- service-worker/background.ts
+++ service-worker/background.ts
@@ -1,14 +1,35 @@
 import * as browser from 'webextension-polyfill'
 
-browser.runtime.onMessage.addListener(async message => {
-  if (message.action === 'download') {
-    const downloadResult = await browser.downloads.download({
-      url: message.url,
-    })
-    return downloadResult
-  }
-})
+;(async () => {
+  const { extensionEnabled } = await browser.storage.sync.get(
+    'extensionEnabled'
+  )
 
-browser.runtime.onMessage.addListener(message => {
-  console.log(message)
-})
+  if (extensionEnabled === undefined) {
+    browser.storage.sync.set(
+      {'extensionEnabled': true}
+    )
+  }
+
+  console.log(extensionEnabled)
+
+  browser.action.setBadgeText({ text: 'hi' })
+
+  browser.action.onClicked.addListener(() => {
+    // TODO: toggle
+    console.log('clicked')
+  })
+
+  browser.runtime.onMessage.addListener(async message => {
+    if (message.action === 'download') {
+      const downloadResult = await browser.downloads.download({
+        url: message.url,
+      })
+      return downloadResult
+    }
+  })
+
+  browser.runtime.onMessage.addListener(message => {
+    console.log(message)
+  })
+})()
